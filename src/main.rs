d1b7eef64f4058903b5110c8a2d770293f82ebfa
Teun van Boxtel <teunvboxtel@hotmail.nl>
2021-01-15 18:36:36 +0100

    feat: refactor logic
    - [x] refactor config into struct and method
    - [ ] handle errors properly

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
 use std::env;
-
+use std::fs;
 fn main() {
-    let vars: Vec<String> = env::args().collect();
-    println!("Vars: {:?}", vars)
+    let vars: Vec<String> = env::args().skip(1).collect();
+    let config = Config::new(&vars);
+
+    let contents = fs::read_to_string(config.filename)
+        .expect("Somethign went wrong while reading the string.");
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if (args.len() < 2) {
+            panic!("Not enough arguments")
+        }
+        let query = args[0].clone();
+        let filename = args[1].clone();
+
+        Config { query, filename }
+    }
 }
