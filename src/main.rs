92b84bc284a04e5c41db215bc71d47e51ffd0d9b
Teun van Boxtel <35037125+Tevabo@users.noreply.github.com>
2021-01-17 14:57:37 +0100

    WIP: feat: refactor logic (#1)
    * feat: refactor logic
    
    - [x] refactor config into struct and method
    - [x ] handle errors properly
    - [x] split into main and lib files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 use std::env;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
-    let vars: Vec<String> = env::args().collect();
-    println!("Vars: {:?}", vars)
+    let vars: Vec<String> = env::args().skip(1).collect();
+    let config = Config::new(&vars).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1)
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1)
+    }
 }
